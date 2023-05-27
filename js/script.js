f7fb4baa44f68582ad7dbd4c1b8ede5bd666cf0f
Jay <j.adeyina@gmail.com>
2023-05-27 20:53:25 +0900

    Added JavaScript code

--- js/script.js
+++ js/script.js
@@ -2,7 +2,18 @@ const share = document.querySelector('.share');
 const shareDark = document.querySelector('.share-dark');
 const overlay = document.querySelector('.overlay');
 
-const handleClick = () => overlay.classList.remove('hide');
+const handleClick = () => {
+    if (overlay.classList.contains('hide')) {
+        overlay.classList.remove('hide');
+        share.style.backgroundColor = "#48556A";
+        share.style.color = "#FFF";
+    } else {
+        overlay.classList.add('hide');
+        share.style.backgroundColor = "#ECF2F8";
+        share.style.color = "#48556A";
+    }
+}
+
 const handleDarkClick = () => overlay.classList.add('hide');
 share.addEventListener('click', handleClick);
 shareDark.addEventListener('click', handleDarkClick);
