28d70cce8df1f6116e34b303d2e851cde84b76b2
Jay <j.adeyina@gmail.com>
2023-05-28 06:02:41 +0900

    Added margin styles

--- js/script.js
+++ js/script.js
@@ -5,12 +5,12 @@ const overlay = document.querySelector('.overlay');
 const handleClick = () => {
     if (overlay.classList.contains('hide')) {
         overlay.classList.remove('hide');
-        share.style.backgroundColor = "#48556A";
-        share.style.color = "#FFF";
+        // share.style.backgroundColor = "#48556A";
+        // share.style.color = "#FFF";
     } else {
         overlay.classList.add('hide');
-        share.style.backgroundColor = "#ECF2F8";
-        share.style.color = "#48556A";
+        // share.style.backgroundColor = "#ECF2F8";
+        // share.style.color = "#48556A";
     }
 }
 
