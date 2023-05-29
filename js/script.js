44d2563dc953d857b4abc78bd612866595b676bb
Jay <j.adeyina@gmail.com>
2023-05-29 17:23:22 +0900

    Change button functionality

--- js/script.js
+++ js/script.js
@@ -2,18 +2,25 @@ const share = document.querySelector('.share');
 const shareDark = document.querySelector('.share-dark');
 const overlay = document.querySelector('.overlay');
 
-const handleClick = () => {
-    if (overlay.classList.contains('hide')) {
-        overlay.classList.remove('hide');
-        // share.style.backgroundColor = "#48556A";
-        // share.style.color = "#FFF";
-    } else {
-        overlay.classList.add('hide');
-        // share.style.backgroundColor = "#ECF2F8";
-        // share.style.color = "#48556A";
-    }
+// const handleClick = () => {
+    
+// }
+
+const handleDarkClick = () => {
+    
 }
 
-const handleDarkClick = () => overlay.classList.add('hide');
-share.addEventListener('click', handleClick);
-shareDark.addEventListener('click', handleDarkClick);
+// const handleDarkClick = () => overlay.classList.add('hide');
+
+share.addEventListener('click', function() {
+    overlay.classList.remove('hide');
+    share.classList.add("hide");
+    shareDark.classList.remove("hide");
+    console.log('it works');
+});
+
+shareDark.addEventListener('click', function() {
+    overlay.classList.add('hide');
+    shareDark.classList.add('hide');
+    share.classList.remove('hide');
+});
